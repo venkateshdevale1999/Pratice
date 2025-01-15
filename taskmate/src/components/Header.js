96db026f3dcb8b9cab290d5cfdac5bbee64467b9
venkateshdevale1999 <89782786+venkateshdevale1999@users.noreply.github.com>
2025-01-15 14:31:32 -0500

    commit done at 1/15/2025 at 2.31pm

---
+++ taskmate/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from "../assests/logo.png"
+import "./Header.css"
+
+export const Header = () => {
+  return (
+    <div>
+      <img src={Logo} className='logo' alt="" />
+
+
+    </div>
+  )
+}
